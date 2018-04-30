54a78d8a4ba499534d2515e4965b8f1690edc879
Kurnia D Win <kurnia.d.win@gmail.com>
2018-04-30 13:08:53 +0700

    update installer script

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-curl -sSLfo /usr/local/bin/docker.sh https://raw.githubusercontent.com/payfazz/docker-sh/master/docker.sh
+download=y
+
+if [ "$(basename "$0")" = "install.sh" ]; then
+    file="$(dirname "$0")/docker.sh"
+    if [ -f "$file" ]; then
+        cp "$file" /usr/local/bin/docker.sh && download=n
+    fi
+fi
+
+[ $download = y ] && curl -sSLfo /usr/local/bin/docker.sh https://raw.githubusercontent.com/payfazz/docker-sh/master/docker.sh
 chmod 755 /usr/local/bin/docker.sh
 
 exit 0
