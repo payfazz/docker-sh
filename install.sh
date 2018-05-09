e8eddff0c969926568451d44845ba84570b243ca
Kurnia D Win <kurnia.d.win@gmail.com>
2018-05-09 11:55:31 +0700

    install.sh to custom location

--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 set -eu
 
 download=y
+target=${1:-/usr/local/bin/docker.sh}
 
 if [ "$(basename "$0")" = "install.sh" ]; then
     file="$(dirname "$0")/docker.sh"
     if [ -f "$file" ]; then
-        cp "$file" /usr/local/bin/docker.sh && download=n
+        cp "$file" "$target" && download=n
     fi
 fi
 
-[ $download = y ] && curl -sSLfo /usr/local/bin/docker.sh https://raw.githubusercontent.com/payfazz/docker-sh/master/docker.sh
-chmod 755 /usr/local/bin/docker.sh
+[ $download = y ] && curl -sSLfo "$target" https://raw.githubusercontent.com/payfazz/docker-sh/master/docker.sh
+chmod 755 "$target"
 
 exit 0
